631eb9a9dc3e4fc28abf403ccbcbae1b7902c13c
hades-rp2010 <40086973+hades-rp2010@users.noreply.github.com>
2026-01-11 20:22:17 +0000

    Deploying to gh-pages from @ hades-rp2010/hades-rp2010.github.io@f1347fe6af314e1092b3e934ba5215b6a0562aeb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,42 +7,42 @@ ninja.data = [{
     title: "about",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/new_site.github.io/";
+      window.location.href = "/";
     },
   },{id: "nav-publications",
           title: "publications",
           description: "publications by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/new_site.github.io/publications/";
+            window.location.href = "/publications/";
           },
         },{id: "nav-interests",
           title: "interests",
           description: "Research interests and areas of focus.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/new_site.github.io/interests/";
+            window.location.href = "/interests/";
           },
         },{id: "nav-bookshelf",
           title: "bookshelf",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/new_site.github.io/books/";
+            window.location.href = "/books/";
           },
         },{id: "nav-papershelf",
           title: "papershelf",
           description: "Papers I am reading or finding interesting.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/new_site.github.io/papershelf/";
+            window.location.href = "/papershelf/";
           },
         },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/new_site.github.io/blog/";
+            window.location.href = "/blog/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
@@ -70,12 +70,12 @@ ninja.data = [{
           title: 'Book List',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/new_site.github.io/books/list/";
+              window.location.href = "/books/list/";
             },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/new_site.github.io/news/announcement_2/";
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -86,47 +86,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/new_site.github.io/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -160,7 +160,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/new_site.github.io/assets/pdf/rishabh_cv2025.pdf", "_blank");
+          window.open("/assets/pdf/rishabh_cv2025.pdf", "_blank");
         },
       },{
       id: 'light-theme',
